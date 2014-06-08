aa8a959774ec8f14519c7749d6afd01b48b6fc82
Mike Calvanese <mfcalvanese@gmail.com>
2014-06-08 13:38:35 -0400

    created jsloader task file
    moved Gruntfile.js to tests dir

---
+++ tasks/jsloader.js
@@ -0,0 +1,21 @@
+/*
+ * grunt-jsloader
+ * https://github.com/mikec/grunt-jsloader
+ *
+ * Copyright (c) 2014 Mike Calvanese
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+    grunt.registerMultiTask(
+        'jsloader',
+        'Set up scripts for asynchronous loading',
+        function() {
+
+        }
+    );
+
+};
